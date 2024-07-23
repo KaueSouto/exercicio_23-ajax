c3444304a36baad87a4451eeda44d3d07473d2e0
Kaue <kauesouto7@gmail.com>
2024-07-23 20:26:36 -0300

    commit inicial

---
+++ ajax.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const avatar = document.querySelector('#avatar');
+    const name = document.querySelector('#name');
+    const userName = document.querySelector('#user-name');
+    const repositories = document.querySelector('#repositories');
+    const followers = document.querySelector('#followers');
+    const following = document.querySelector('#following');
+    const userLink = document.querySelector('#user-link');
+
+    fetch('https://api.github.com/users/kauesouto')
+    .then(function(r){
+        return r.json();
+    })
+    .then(function(json){
+        avatar.src = json.avatar_url;
+        name.innerText = json.name;
+        userName.innerText = json.login;
+        repositories.innerText = json.public_repos;
+        followers.innerText = json.followers;
+        following.innerText = json.following;
+        userLink.href = json.html_url;
+    })
+})
